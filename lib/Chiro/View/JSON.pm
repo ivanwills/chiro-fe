3ea8685df37ce489dcddf06e722f37cca9d7df0e
Ivan Wills <ivan.wills@gmail.com>
2016-07-01 20:13:24 +1000

    Added JSON view

---
+++ lib/Chiro/View/JSON.pm
@@ -0,0 +1,31 @@
+package Chiro::View::JSON;
+use Moose;
+use namespace::autoclean;
+
+extends 'Catalyst::View';
+
+=head1 NAME
+
+Chiro::View::JSON - Catalyst View
+
+=head1 DESCRIPTION
+
+Catalyst View.
+
+
+=encoding utf8
+
+=head1 AUTHOR
+
+Ivan Wills,,,
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
---
+++ t/view_JSON.t
@@ -0,0 +1,8 @@
+use strict;
+use warnings;
+use Test::More;
+
+
+BEGIN { use_ok 'Chiro::View::JSON' }
+
+done_testing();
