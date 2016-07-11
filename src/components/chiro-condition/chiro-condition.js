03c8e72d1b039a1e9e8309b870c3d45f97cb8c1a
Ivan Wills <ivan.wills@gmail.com>
2016-07-11 19:31:04 +1000

    Added reset of fields

--- src/components/chiro-condition/chiro-condition.js
+++ src/components/chiro-condition/chiro-condition.js
@@ -2,6 +2,26 @@
 Ractive.extend({
     template: Ractive.defaults.templates['chiro-condition'],
     isolated: true,
+    data: () => {
+        return {
+            fields: [
+                { field: 'condition', label: 'Condition' },
+                { field: 'condition_desc', label: 'Description' },
+                { field: 'condition_description_source', label: 'Description Source' },
+                { field: 'clasification_type', label: 'Classification' },
+                { field: 'clasification_sub_type', label: 'Subtype' },
+                { field: 'chiro', label: 'Chiro' },
+                { field: 'age', label: 'Age' },
+                { field: 'gender', label: 'Gender' },
+                { field: 'etiology', label: 'Etiology' },
+                { field: 'ddx', label: 'DDX' },
+                { field: 'comorbid', label: 'Comorbid' },
+                { field: 'demographics', label: 'Demographics' },
+                { field: 'grouping', label: 'Grouping' },
+                { field: 'mechanism', label: 'Mechanism' },
+            ]
+        };
+    },
     onrender: function() {
         this.observe('condition', function(newVal) {
             if (!newVal) {
